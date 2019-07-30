0b0c0d92f4bd3dee3b992f4c5b34e3406c0cd47f
anjali-r041196 <anjalir041196@gmail.com>
2019-07-30 16:23:55 +0530

    pp

--- g.js
+++ g.js
@@ -2,17 +2,31 @@ $(document).ready(function(){
     $("#get").click(function(){
         $.ajax({
             type:"GET",
-            url:"data.json",
+            url:"https://jsonplaceholder.typicode.com/users",
             success:function(data){
-               var out="<table class='table'><thead><tr><th>name</th><th>dob</th><th>college</th><th>mobile</th></tr></thead>"
-                   for(var i in data){
-                   out+="<tr><td>"+data[i].name+"</td>"
-                   out+="<td>"+data[i].dob+"</td>"
-                   out+="<td>"+data[i].college+"</td>"
-                   out+="<td>"+data[i].mobile+"</td></tr>"
+                
+           
+                   
+                    var out=""
+                
+                for(var i in data){
+                   
+                    out  +=  "<div class='card'><div class='card-header'>name</div>"
+                   
+                        
+                   out+="id: "+data[i].id+"<td>"
+                   out+="<td>name: "+data[i].name+"</td>"
+                   out+="<td>username:"+data[i].username+"</td>"
+                   out+="<td>"+data[i].address.street+"</td>"
+                   out+="<td>"+data[i].address.suite+"</td></tr>"
+                         
+
+                   out+="</div> <br> "                            
+                   $(".result").html(out);
+
 }
-out+="</table>"
-$(".result").html(out);
+
+
             }
         })
     })
